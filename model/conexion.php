9dc764694f3a5eac87130c8bb0805b02da27bb4b
AlxDev001 <worfysito15@gmail.com>
2025-01-06 16:02:20 -0500

    feat: cambios detectados nueva funcionalidad en conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,55 +1,25 @@
 <?php
 
 ///hostin local
- 
+
 class Conexion
 {
   static public function conn()
   {
-    $link = new PDO("mysql:host=localhost;dbname=bd_dfrida", "root", "");
+    $link = new PDO("mysql:host=db;dbname=pruebasbetaacide_dfrida", "root", "test");
     $link->exec("set names utf8");
     return $link;
   }
 }
 
-//hostinguer acide
+//hostinguer
 
 /* class Conexion
 {
   static public function conn()
   {
-    $link = new PDO("mysql:host=localhost;dbname=u553466910_dfrida", "u553466910_root_dfrida", "^Mhq*Q6sp$0l");
+    $link = new PDO("mysql:host=localhost;dbname=pruebasbetaacide_admin_dfrida", "pruebasbetaacide_admin_dfrida", "xb{kk[6{uMrF");
     $link->exec("set names utf8");
     return $link;
   }
 } */
-
-//buscar usuario en mysql hostinger ACIDE
-//SELECT USER();
-// 
-//credenciales conexion remota
-//host:154.56.48.204
-//usu:u553466910_root_dfrida
-//pass:^Mhq*Q6sp$0l
-
-
-///
-
-//hostinguer pruebas unitarias
-
-/* class Conexion
-{
-  static public function conn()
-  {
-    $link = new PDO("mysql:host=localhost;dbname=u993966586_dfrida", "u993966586_dfrida", "#Dfrida1234");
-    $link->exec("set names utf8");
-    return $link;
-  }
-} */
-//buscar usuario en mysql hostinger
-//SELECT USER();
-// 
-//credenciales conexion remota
-//host: 193.203.175.106
-//usu: u993966586_dbdfrida
-//pass: #Dfrida1234
