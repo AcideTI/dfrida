73fe6d49d720343762989e369e36f226273d7cef
AlxDev001 <worfysito15@gmail.com>
2025-01-07 15:37:58 -0500

    feat: cambios detectados nueva funcionalidad en almacenProductos.model.php

--- model/almacenProductos.model.php
+++ model/almacenProductos.model.php
@@ -8,7 +8,7 @@ class almacenProductosModel
   public static function mdlDTableAlmacenProductos($table)
   {
     $statement = Conexion::conn()->prepare("SELECT 
-      idAlmaProd, idProd, codigoProdAlma, nombreProdAlma, unidadProdAlma, cantidadProdAlma, precioProdAlma, DateUpdate 
+      idAlmaProd, idProd, codigoProdAlma, nombreProdAlma, unidadProdAlma, cantidadProdAlma, precioProdAlma, DateUpdate, precioUnit
         FROM $table 
         ORDER BY 
           CASE 
@@ -22,5 +22,27 @@ class almacenProductosModel
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
-  
+
+  //funcion para obtener el precio unitario de un producto
+  public static function mdlPrecioUnitarioProducto($table, $idProd)
+  {
+    $statement = Conexion::conn()->prepare("SELECT precioProd FROM $table WHERE idProd = :idProd");
+    $statement->bindParam(":idProd", $idProd, PDO::PARAM_INT);
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    return $result ? $result['precioProd'] : null;
+  }
+
+  //actualizar precio unitario de un producto
+  public static function mdlUpdateUnitarioProducto($table, $idProd, $precioUnit)
+  {
+    $statement = Conexion::conn()->prepare("UPDATE $table SET precioUnit = :precioUnit WHERE idProd = :idProd");
+    $statement->bindParam(":idProd", $idProd, PDO::PARAM_INT);
+    $statement->bindParam(":precioUnit", $precioUnit, PDO::PARAM_STR);
+    if ($statement->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
